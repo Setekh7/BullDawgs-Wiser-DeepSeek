0b86529f9e0553f1bd5643507c23064cb0006dce
Aden Carter <157550210+acc858@users.noreply.github.com>
2025-02-26 19:59:03 -0600

    Update CourseGraph.ts

--- src/app/lib/CourseGraph.ts
+++ src/app/lib/CourseGraph.ts
@@ -99,6 +99,18 @@ courseGraph.addCourse("CSE 4223", ["CSE 4214"]); // Manage SW Project
 courseGraph.addCourse("CSE 3763", []); // Legal and Ethical Issues
 courseGraph.addCourse("CSE 3223", ["CSE 4214"]); // SE Senior Project 2
 
+//Required Sciences
+// Level 1 Science Courses
+courseGraph.addCourse("1113", []);
+courseGraph.addCourse("1213", []);
+courseGraph.addCourse("1134", []);
+
+
+// Level 2 Science Courses 
+courseGraph.addCourse("1123", ["1113"]); // Requires 1113
+courseGraph.addCourse("1223", ["1213"]); // Requires 1213
+courseGraph.addCourse("1144", ["1134"]); // Requires 1134
+
 // Example usage
 //const completed = ["CSE 1284", "CSE 1384", "CSE 2383"];
 //console.log("Available Courses:", courseGraph.getAvailableCourses(completed));
